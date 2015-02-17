196b48571ef071e8a5b371aaf48dffeabc12dedc
softprops <d.tangren@gmail.com>
2015-02-17 01:15:27 -0500

    parse some things out

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 #[macro_use] extern crate nickel_macros;
 extern crate nickel;
 extern crate hyper;
+extern crate "rustc-serialize" as serialize;
 
+use serialize::json::Json;
 use std::old_io::net::ip::Ipv4Addr;
 use nickel::{ Nickel, Request, Response, HttpRouter };
 use nickel::mimes::MediaType;
@@ -17,7 +19,13 @@ fn main() {
         .ok().expect("invalid url");
       match Client::new().get(uri).send() {
         Ok(mut res) => {
-           //println!("{:?}", res.read_to_string().ok().expect("no body"));
+           let payload = res.read_to_string().ok()
+               .and_then(|s| Json::from_str(&s).ok());
+           let max_version = match payload {
+             Some(ref j) => j.find("crate"),
+             _ => None
+           }.and_then(|j| j.find("max_version"));
+           println!("{:?}", max_version);
           _res.content_type(MediaType::Svg);
           _res.send(format!("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"112\" height=\"20\"><linearGradient id=\"b\" x2=\"0\" y2=\"100%\"><stop offset=\"0\" stop-color=\"#bbb\" stop-opacity=\".1\"/><stop offset=\"1\" stop-opacity=\".1\"/></linearGradient><mask id=\"a\"><rect width=\"112\" height=\"20\" rx=\"3\" fill=\"#fff\"/></mask><g mask=\"url(#a)\"><path fill=\"#555\" d=\"M0 0h59v20H0z\"/><path fill=\"#fe7d37\" d=\"M59 0h53v20H59z\"/><path fill=\"url(#b)\" d=\"M0 0h112v20H0z\"/></g><g fill=\"#fff\" text-anchor=\"middle\" font-family=\"DejaVu Sans,Verdana,Geneva,sans-serif\" font-size=\"11\"><text x=\"30.5\" y=\"15\" fill=\"#010101\" fill-opacity=\".3\">{crate}</text><text x=\"30.5\" y=\"14\">{crate}</text><text x=\"84.5\" y=\"15\" fill=\"#010101\" fill-opacity=\".3\">v0.2.13</text><text x=\"84.5\" y=\"14\">v0.2.13</text></g></svg>", crate = _req.param("crate")))
         },
