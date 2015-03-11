6b74572c06aa1675130cce4261f644cc06f1d99b
softprops <d.tangren@gmail.com>
2015-03-11 09:00:04 -0400

    spacing

--- src/main.rs
+++ src/main.rs
@@ -14,39 +14,39 @@ use rustc_serialize::json::Json;
 use std::io::Read;
 use handlebars_iron::{ HandlebarsEngine, Template };
 
-fn labeled(req: &mut Request) -> IronResult<Response> {
-    let ref krate = req.extensions.get::<Router>()
-            .unwrap().find("crate").unwrap_or("/");
-    let uri = Url::parse(
-        &*format!("https://crates.io/api/v1/crates/{}", krate))
-        .ok().expect("invalid url");
-    match Client::new().get(uri).send() {
-      Ok(mut res) => {
-        let mut s = String::new();
-        match res.read_to_string(&mut s)
-          .map(|_| s).ok()
-          .and_then(|s| Json::from_str(&s).ok()) {
-          Some(data) => {
-            let mut res = Response::new();
-            res.set_mut(iron::status::Ok);
-            res.headers.set(ContentType("image/svg+xml".parse().unwrap()));
-            res.set_mut(Template::new("label", data));
-            Ok(res)
-          },
-                    _ =>
-         Ok(Response::with((iron::status::Ok, "???")))
-       }
-     },
-     Err(e) => {
-       let err = format!("Err: {:?}", e);
-       Ok(Response::with((iron::status::Ok, err)))
-     }
-   }
+fn label(req: &mut Request) -> IronResult<Response> {
+  let ref krate = req.extensions.get::<Router>()
+    .unwrap().find("crate").unwrap_or("/");
+  let uri = Url::parse(
+    &*format!("https://crates.io/api/v1/crates/{}", krate))
+    .ok().expect("invalid url");
+  match Client::new().get(uri).send() {
+    Ok(mut res) => {
+      let mut s = String::new();
+      match res.read_to_string(&mut s)
+        .map(|_| s).ok()
+        .and_then(|s| Json::from_str(&s).ok()) {
+        Some(data) => {
+          let mut res = Response::new();
+          res.set_mut(iron::status::Ok);
+          res.headers.set(ContentType("image/svg+xml".parse().unwrap()));
+          res.set_mut(Template::new("label", data));
+          Ok(res)
+        },
+                 _ =>
+        Ok(Response::with((iron::status::Ok, "???")))
+      }
+    },
+    Err(e) => {
+      let err = format!("Err: {:?}", e);
+      Ok(Response::with((iron::status::Ok, err)))
+    }
+  }
 }
 
 fn main() {  
   let mut router = Router::new();
-  router.get("/labels/:crate", labeled);
+  router.get("/labels/:crate", label);
   let mut chain = Chain::new(router);
   chain.link_after(HandlebarsEngine::new("./src/templates/", ".hbs"));
   Iron::new(chain).http("localhost:3000").unwrap();
